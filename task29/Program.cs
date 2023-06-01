a0f86ab0b5781ca74f757cba96efa740b6efbd20
Красавчик <stech999@ya.ru>
2023-06-01 22:37:05 +0300

    решение готово

--- task29/Program.cs
+++ task29/Program.cs
@@ -2,3 +2,21 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33-> [6, 1, 33]
 
+Console.Clear();
+
+void tet (int num)
+{
+    int[] dig = new int[num];
+    Random number = new Random(num);
+
+    for (int i = 0; i < dig.Length; i++)
+    {
+        dig[i] = number.Next(num);
+        Console.Write(dig[i] + " ");
+    }
+    
+}
+
+Console.WriteLine("Сколько вывести элементов? ");
+int kek = int.Parse(Console.ReadLine()!);
+tet(kek);
